430b6828e1808764248281d96643b63378121379
Guui360gpa <guipaiva.alves14@gmail.com>
2026-08-18 16:24:01 -0300

    add: modeling the TicketBatch entity

--- src/main/java/com/contatodireto/eventflow/model/TicketBatch.java
+++ src/main/java/com/contatodireto/eventflow/model/TicketBatch.java
@@ -5,10 +5,45 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Entity
 @RequiredArgsConstructor
 @Getter
 @Setter
 public class TicketBatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event_id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer totQuantity;
+
+    @Column(nullable = false)
+    private Integer totAvailable;
+
+    @Column(nullable = false)
+    private Integer order;
+
+    @Column(nullable = false)
+    private LocalDateTime saleStart;
+
+    @Column(nullable = false)
+    private LocalDateTime saleEnd;
+
+    @Column(nullable = false)
+    private Integer version;
 
 }
