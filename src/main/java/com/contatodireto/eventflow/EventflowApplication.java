8dab6259cb18f0ca981224f01c6ccb0b56cc5c8b
Bernardo Novaes <bernardo@contatodireto.com>
2026-08-18 15:18:11 -0300

    feat: initialize project eventflow

--- src/main/java/com/contatodireto/eventflow/EventflowApplication.java
+++ src/main/java/com/contatodireto/eventflow/EventflowApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventflowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventflowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventflowApplication.class, args);
+    }
 
 }
--- src/test/java/com/contatodireto/eventflow/TestEventflowApplication.java
+++ src/test/java/com/contatodireto/eventflow/TestEventflowApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestEventflowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(EventflowApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(EventflowApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
 
 }
