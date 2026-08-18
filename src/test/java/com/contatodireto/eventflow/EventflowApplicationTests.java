55bbf92995739b118724d20bedcc2f86ad807068
Bernardo Novaes <bernardo@contatodireto.com>
2026-08-18 15:12:38 -0300

    Start project

---
+++ src/test/java/com/contatodireto/eventflow/EventflowApplicationTests.java
@@ -0,0 +1,15 @@
+package com.contatodireto.eventflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class EventflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/contatodireto/eventflow/TestEventflowApplication.java
@@ -0,0 +1,11 @@
+package com.contatodireto.eventflow;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestEventflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(EventflowApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
