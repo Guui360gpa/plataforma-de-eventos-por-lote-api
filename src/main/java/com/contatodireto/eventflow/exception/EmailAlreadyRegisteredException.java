832950b4909e9fa6073e01f260992bb6b260e212
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-10 17:48:32 -0300

    feat: Adds an exception for an already registered email.

--- src/main/java/com/contatodireto/eventflow/exception/EmailAlreadyRegisteredException.java
+++ src/main/java/com/contatodireto/eventflow/exception/EmailAlreadyRegisteredException.java
@@ -1,7 +1,7 @@
 package com.contatodireto.eventflow.exception;
 
 public class EmailAlreadyRegisteredException extends RuntimeException {
-  public EmailAlreadyRegisteredException(String message) {
-    super(message);
-  }
+    public EmailAlreadyRegisteredException(String message) {
+        super(message);
+    }
 }
