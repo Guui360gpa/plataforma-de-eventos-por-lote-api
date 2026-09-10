879fa12dfaed48f87530b258c0f74c3eb1673db1
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-10 16:59:57 -0300

    feat: Adds the repository for the Registration entity.

--- src/main/java/com/contatodireto/eventflow/repository/RegistrationRepository.java
+++ src/main/java/com/contatodireto/eventflow/repository/RegistrationRepository.java
@@ -1,4 +1,9 @@
 package com.contatodireto.eventflow.repository;
 
-public interface RegistrationRepository {
+import jakarta.servlet.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 }
