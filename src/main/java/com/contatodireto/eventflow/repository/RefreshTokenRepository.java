38f57f8281f1d048b9705569d109a5c50742e6c9
Bernardo Novaes <bernardo@contatodireto.com>
2026-09-10 16:56:39 -0300

    feat: RefreshTokenRepository findbyToken & find by revoked false

---
+++ src/main/java/com/contatodireto/eventflow/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.contatodireto.eventflow.repository;
+
+import com.contatodireto.eventflow.model.RefreshToken;
+import com.contatodireto.eventflow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    List<RefreshToken> findAllByUserAndRevokedFalse(User user);
+}
